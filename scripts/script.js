cd2eb978fa2bac85cdcea8cce53288013478705e
Kirstyn Blandy <kirstyn.blandy@gmail.com>
2019-06-13 17:36:43 -0400

    Fixed margin collapse, added additional pages.

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+//want to make a script that logs user input in join form into console
+//when user logs in, information should match what is in console
+//if not, decline entry
+
+const joinForm = document.getElementById('join-form');
+const user = {};//create an empty object
+const userInfo = document.getElementById(user.['']);
+
+info.username = document.getElementById('user_name').value;
+info.doB = document.getElementById('user_date').value;
+info.email = document.getElementById('user_email').value;
+info.password = document.getElementById('user_password').value;
+
+
+
+
+
+joinForm.submit.addEventListener('click', (event) =>{
+        event.preventDefault();
+        console.log(info);
+        });
